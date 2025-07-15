92de8df3515e419974036f050e02e2fd7f081a26
Suraj Pantha <surajpantha82@gmail.com>
2025-07-15 14:42:53 +0545

    currency converter project using custom hook

---
+++ 06currencyconverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+    useEffect(() => {
+        const fetchCurrencyData = async () => {
+            try {
+                const response = await fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json
+`);
+                const result = await response.json();
+                // console.log(result)
+                setData(result[currency])
+
+
+
+            }
+
+
+            catch (error) {
+                console.error("error fetching data:", error)
+                setData({})
+            }
+        }
+        fetchCurrencyData()
+    }, [currency]);
+    return data;
+}
+
+export default useCurrencyInfo
